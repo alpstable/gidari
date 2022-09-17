311f3974fbc648bbd9ba214f1b43bff65dcdd28e
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-17 13:54:47 -0600

    setup docker-compose

---
+++ scripts/update-etc-hosts.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# declare an array of hosts to add to 127.0.0.1
+hosts=("mongo" "mongo1" "mongo2" "postgres-coinbasepro" "postgres-polyon" "redis1")
+
+for i in "${hosts[@]}"
+do
+	if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+	then
+		echo "$i already exists: $(grep $i /etc/hosts)"
+	else
+		sudo -- sh -c -e "echo '127.0.0.1 $i' >> /etc/hosts"
+	fi
+done
