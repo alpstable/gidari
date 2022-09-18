b97193a3bce98cb1d1fbf863ff5794080692c4d1
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-18 01:20:17 -0600

    update ci/cd

--- scripts/run-ctests.sh
+++ scripts/run-ctests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-docker-compose -f "third_party/docker/storage.docker-compose.yaml" up -d \
-	--remove-orphans \
-	--force-recreate \
-	--build ctests
-
-docker-compose -f "third_party/docker/storage.docker-compose.yaml" run --rm ctests
+#docker-compose -f "third_party/docker/storage.docker-compose.yaml" up -d \
+#	--remove-orphans \
+#	--force-recreate \
+#	--build ctests
+#
+echo "meepman: $(MEEPMAN)"
+#docker-compose -f "third_party/docker/storage.docker-compose.yaml" run --rm \
+#	-e "CTEST_PARALLEL_LEVEL=8" \
+#	ctests
 
