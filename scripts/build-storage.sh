7a932b06bd1ec4e0ff601dc0552951cfe4b95316
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-18 17:43:41 -0600

    more ci

--- scripts/build-storage.sh
+++ scripts/build-storage.sh
@@ -30,33 +30,7 @@ echo "Waiting for MongoDB to start..."
 sleep 5
 
 echo "Creating replica set..."
-docker-compose -f "docker-compose.yml" exec -T mongo1 bash -c '
-mongosh <<EOF
-var config = {
-    "_id": "dbrs",
-    "version": 1,
-    "members": [
-        {
-            "_id": 1,
-            "host": "mongo1:27017",
-            "priority": 3
-        },
-        {
-            "_id": 2,
-            "host": "mongo2:27017",
-            "priority": 2
-        },
-        {
-            "_id": 3,
-            "host": "mongo3:27017",
-            "priority": 1
-        }
-    ]
-};
-rs.initiate(config, { force: true });
-rs.status();
-EOF
-'
+docker-compose -f "docker-compose.yml" exec -T mongo1 scripts/rs-init.sh
 
 docker-compose -f "docker-compose.yml" up -d \
 	--remove-orphans \
