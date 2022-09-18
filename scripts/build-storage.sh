28ade100ccab37041ab2960b1bae850bf2c90d45
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-18 00:03:22 -0600

    update ci/cd

--- scripts/build-storage.sh
+++ scripts/build-storage.sh
@@ -30,10 +30,36 @@ docker-compose -f "third_party/docker/storage.docker-compose.yaml" up -d \
 	--build mongo3
 
 echo "Waiting for MongoDB to start..."
-sleep 15
+sleep 5
 
 echo "Creating replica set..."
-sudo docker-compose -f "third_party/docker/storage.docker-compose.yaml" exec mongo1 /scripts/rs-init.sh
+docker-compose -f "third_party/docker/storage.docker-compose.yaml" exec -T mongo1 bash -c '
+mongosh <<EOF
+var config = {
+    "_id": "dbrs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 1,
+            "host": "mongo1:27017",
+            "priority": 3
+        },
+        {
+            "_id": 2,
+            "host": "mongo2:27017",
+            "priority": 2
+        },
+        {
+            "_id": 3,
+            "host": "mongo3:27017",
+            "priority": 1
+        }
+    ]
+};
+rs.initiate(config, { force: true });
+rs.status();
+EOF
+'
 
 
 docker-compose -f "third_party/docker/storage.docker-compose.yaml" up -d \
