60be3499907e07ec8443f0e1110e143302f1093a
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-18 11:47:09 -0600

    update ci/cd

--- scripts/build-migrations.sh
+++ scripts/build-migrations.sh
@@ -3,8 +3,8 @@
 set -e
 
 docker-compose -f "third_party/docker/storage.docker-compose.yaml" run liquibase-postgres-coinbasepro \
-	--changelog-file=/changelog/changelog.xml \
-	--url=jdbc:postgresql://postgres-coinbasepro:5432/coinbasepro \
+	--changelog-file="/changelog/changelog.xml" \
+	--url="jdbc:postgresql://postgres-coinbasepro:5432/coinbasepro" \
 	--log-level=debug \
 	--username=postgres \
 	--driver=org.postgresql.Driver update
