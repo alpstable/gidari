738a90163f67e11ac9debd7c5d3c028a9c93db63
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-17 23:32:52 -0600

    update ci/cd

--- scripts/build-storage.sh
+++ scripts/build-storage.sh
@@ -31,7 +31,9 @@ echo "Waiting for MongoDB to start..."
 sleep 15
 
 echo "Creating replica set..."
-sudo docker exec --privileged docker-mongo1-1 /scripts/rs-init.sh
+# sudo docker exec --privileged docker-mongo1-1 /scripts/rs-init.sh
+docker-compose -f "third_party/docker/storage.docker-compose.yaml" exec mongo1 /scripts/rs-init.sh
+
 
 docker-compose -f "third_party/docker/storage.docker-compose.yaml" up -d \
 	--remove-orphans \
