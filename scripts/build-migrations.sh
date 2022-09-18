4c866f0dbe1c107cff3f3a38a2aec75ec82a4f22
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2022-09-17 21:19:40 -0600

    update ci/cd

--- scripts/build-migrations.sh
+++ scripts/build-migrations.sh
@@ -12,12 +12,12 @@ docker-compose -f "third_party/docker/storage.docker-compose.yaml" run liquibase
 #	--url=mongodb://mongo:27017/polygon \
 #	--log-level=debug update
 #
-#docker-compose -f "third_party/docker/storage.docker-compose.yaml" run liquibase-postgres-coinbasepro \
-#	--changelog-file=/changelog/changelog.xml \
-#	--url=jdbc:postgresql://postgres-coinbasepro:5432/coinbasepro \
-#	--log-level=debug \
-#	--username=postgres \
-#	--driver=org.postgresql.Driver update
+docker-compose -f "third_party/docker/storage.docker-compose.yaml" run liquibase-postgres-coinbasepro \
+	--changelog-file=/changelog/changelog.xml \
+	--url=jdbc:postgresql://postgres-coinbasepro:5432/coinbasepro \
+	--log-level=debug \
+	--username=postgres \
+	--driver=org.postgresql.Driver update
 
 # prune containers
 docker system prune --force
