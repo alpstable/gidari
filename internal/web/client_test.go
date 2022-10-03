41f74e917795456f48285e7f2bf7dde5868ec9ad
Alexander Tsapkov <axlts.dev@gmail.com>
2022-10-03 08:30:47 +0600

    add negative cases for basic auth test

--- internal/web/client_test.go
+++ internal/web/client_test.go
@@ -49,6 +49,52 @@ func TestFetchWithBasicAuth(t *testing.T) {
 			t.Fatalf("fetch error: %v", err)
 		}
 	})
+
+	t.Run("authorization failed", func(t *testing.T) {
+		t.Parallel()
+
+		const username = "test@email.com"
+		const password = "test"
+
+		ts := createTestServerWithBasicAuth(username, password)
+		defer ts.Close()
+
+		for _, tcase := range []struct {
+			username, password string
+		}{
+			{username: "wrong", password: "wrong"},
+			{username: username},
+			{password: password},
+			{username: "", password: ""},
+		} {
+			ctx := context.Background()
+
+			basicAuth := auth.NewBasic()
+			basicAuth.SetEmail(tcase.username)
+			basicAuth.SetPassword(tcase.password)
+			basicAuth.SetURL(ts.URL)
+
+			client, err := NewClient(ctx, basicAuth)
+			if err != nil {
+				t.Fatalf("error creating client: %v", err)
+			}
+
+			uri, err := url.Parse(ts.URL)
+			if err != nil {
+				t.Fatalf("error parsing url: %v", err)
+			}
+
+			_, err = Fetch(ctx, &FetchConfig{
+				C:           client,
+				Method:      http.MethodGet,
+				URL:         uri,
+				RateLimiter: rate.NewLimiter(1, 1),
+			})
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+		}
+	})
 }
 
 // createTestServerWithBasicAuth is a helper that creates a httptest.Server with a handler that has basic authentication.
