b101905d0190cf82e1e20e4ecd22c6f0ffc28b52
Joseph Peloquin <41835930+SkyWolf369@users.noreply.github.com>
2022-10-12 22:02:41 -0400

    Update gidari.go
    Co-authored-by: Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>

--- gidari.go
+++ gidari.go
@@ -21,6 +21,7 @@ type Config struct {
 	transport.Config
 }
 
+//noling:godox
 // TODO #265: Remove this routine.
 func NewConfig(ctx context.Context, file *os.File) (*Config, error) {
 	info, err := file.Stat()
