be8609b703318757f7c45485dfed056ac2a8e45d
Fahim Bagar <fahim.bagar@gmail.com>
2023-02-11 19:02:39 +0700

    Update add-license.sh
    Close #365

--- scripts/add-license.sh
+++ scripts/add-license.sh
@@ -20,6 +20,12 @@ LICENSE_TEMPLATE=$(cat <<EOF
 EOF
 )
 
+# NEWLINE is added in all files except doc.go.
+NEWLINE=$(cat <<EOF
+//
+EOF
+)
+
 # append all *.go in this repository with the LICENSE_TEMPLATE
 for file in $(find . -name "*.go" -type f); do
     	# skip files in the EXCLUDE_LIST
@@ -28,20 +34,26 @@ for file in $(find . -name "*.go" -type f); do
     	fi
 
     	# skip files that already have the LICENSE_TEMPLATE
-    	if grep -q "Copyright $YEAR The Gidari Authors." "${file}"; then
-		continue
-    	fi
+#    	if grep -q "Copyright $YEAR The Gidari Authors." "${file}"; then
+#		continue
+#    	fi
 
-	# If the file starts with "// Copied from" then we don't want to prepend
-	# the license.
-	if grep -q "^// Copied from" "${file}"; then
-		continue
-	fi
+      # If the file starts with "// Copied from" then we don't want to prepend
+      # the license.
+      if grep -q "^// Copied from" "${file}"; then
+        continue
+      fi
+
+      sed -i '/^package/,$!d' "${file}"
 
     	# prepend the LICENSE_TEMPLATE to the file
-    	echo "${LICENSE_TEMPLATE}" | cat - "${file}" > /tmp/out && mv /tmp/out "${file}"
+    	if [[ "${file}" == ./doc.go ]]; then
+          printf "%s\n" "${LICENSE_TEMPLATE}" | cat - "${file}" > /tmp/out
+        else
+          printf "%s\n%s\n" "${LICENSE_TEMPLATE}" "${NEWLINE}" | cat - "${file}" > /tmp/out
+      fi
+    	mv /tmp/out "${file}"
 
-	echo "${LICENSE_TEMPLATE}" | cat - "${file}" > /tmp/out && mv /tmp/out "${file}"
 done
 
 
